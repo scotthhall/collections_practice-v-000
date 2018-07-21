18a6339a26b2c9f97af6961e019682faee155b27
Scott Hall <scotthall127@gmail.com>
2018-07-21 22:47:51 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -17,8 +17,7 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-  new_array = [array[0]]
-  new_array << array[2]
-  new_array << array[1]
+  new_array = array
+  array[1], array[2] = array[2], array[1]
   new_array
 end
